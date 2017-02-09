b642bb6d9795e27f92568d3d83fb7d637ed5543e
Thibault Latrille <thibault.latrille@ens-lyon.org>
2017-02-09 14:48:22 +0100

    Sass stylesheet (potageome.css.scss) is compiled using the scss python library

--- potageome.py
+++ potageome.py
@@ -1,7 +1,13 @@
 from dico_appartenance import appartenance
 from dico_plantes_categories import plantes
 import jinja2
+from scss import parser
+compress = False
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('./'))
 template = env.get_template('potageome.html')
-output_from_parsed_template = template.stream(plantes=plantes, appartenance=appartenance).dump('potageome_rendered.html')
+output_from_parsed_template = template.stream(plantes=plantes,
+                                              appartenance=appartenance).dump('potageome_rendered.html')
+css = open("potageome.css", "w")
+css.write(parser.Stylesheet(options=dict(compress=compress)).load('potageome.css.scss'))
+css.close()
