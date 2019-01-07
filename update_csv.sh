8f0eed088b31c56e1d55ccc1a4709ffa24d4fabd
ThibaultLatrille <thibault.latrille@univ-lyon1.fr>
2019-01-07 20:46:38 +0100

    Scripted update of .csv

--- update_csv.sh
+++ update_csv.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-wget https://framacalc.org/monpotagerespeces.csv -O especes.csv
-wget https://framacalc.org/monpotagerassociations.csv -O associations.csv
+wget --no-check-certificate --output-document=associations.csv 'https://docs.google.com/spreadsheet/ccc?key=1Wp_fomhElzCspAxgarp1BstonU0HGA_tNB_U2uNskw0&output=csv&gid=0'
+wget --no-check-certificate --output-document=especes.csv 'https://docs.google.com/spreadsheet/ccc?key=1Wp_fomhElzCspAxgarp1BstonU0HGA_tNB_U2uNskw0&output=csv&gid=537765681'
