7c7e8f5300e456c3ce7d5f65615210cf486ba9f9
slundi <slundi@users.noreply.github.com>
2019-05-21 19:47:55 +0200

    Serveur web (ne traite aucune URL pour l'instant)

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+#web server
+WEB_HOST = 'localhost'
+WEB_PORT = 5555
+STATIC_URL = '/static' #can also be complete URL, ie: https://static.my-domain.com/folder-1/folder-2
+
 #Database host
 DB_HOST = 'localhost'
 DB_NAME = 'monpotager'
