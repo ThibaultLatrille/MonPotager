8f6cb6e325ea5d961f9bed5142f1e6b00981198a
slundi <slundi@users.noreply.github.com>
2019-06-04 22:49:33 +0200

    UI: maquettage page plante (en cours)

--- webserver/views.py
+++ webserver/views.py
@@ -5,9 +5,32 @@ from . import app
 def home_page():
     return render_template('home.html')
 
-@app.route('/<int:post_id>-<string:name>.html')
-def item_page():
-    return 'Plant'
+@app.route('/plant/<int:id>-<string:name>.html')
+@app.route('/plante/<int:id>-<string:name>.html')
+def plant(id, name):
+    print("Plante")
+    return render_template('plant.html')
+
+@app.route('/disease/<int:id>-<string:name>.html')
+@app.route('/maladie/<int:id>-<string:name>.html')
+def maladie(id, name):
+    print("Maladie")
+    return("Maladie")
+    #return render_template('desease.html')
+
+@app.route('/nuisible/<int:id>-<string:name>.html')
+@app.route('/pest/<int:id>-<string:name>.html')
+def pest(id, name):
+    print("Nuisible")
+    return("nuisible")
+    #return render_template('pest.html')
+
+@app.route('/auxiliaire/<int:id>-<string:name>.html')
+@app.route('/helper/<int:id>-<string:name>.html')
+def helper(id, name):
+    print("Auxiliaire")
+    return("auxiliaire")
+    #return render_template('helper.html')
 
 @app.route('/interactions-entre-plantes-legumes-fruits-arbres-fleurs-aromates-maladies-auxiliaires.html')
 def interactions():
